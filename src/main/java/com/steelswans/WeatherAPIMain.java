eacf9cb4d359e222a7b57bcdb5a193730ce2405a
Gideon9876543 <98388179+Gideon9876543@users.noreply.github.com>
2022-02-24 11:53:12 +0000

    Main Updated
    WeatherAPIMain class has been created, linking the ConnectionManager to JSON response.

--- src/main/java/com/steelswans/WeatherAPIMain.java
+++ src/main/java/com/steelswans/WeatherAPIMain.java
@@ -1,7 +1,17 @@
 package com.steelswans;
 
+import org.json.simple.JSONObject;
+
+import java.net.http.HttpResponse;
+
 public class WeatherAPIMain {
+    private static JSONObject jsonObject;
+
+
     public static void main(String[] args) {
+        ConnectionManager cm = ConnectionManager.getConnection("api.openweathermap.org/data/2.5/weather?q=",
+                "London", APIKeyFileReader.readAPIKeyFile("apikey.txt"));
+        jsonObject = Injector.getJSONResponse(cm.getHttpResponse(cm.makeHttpRequest()));
 
     }
 }
