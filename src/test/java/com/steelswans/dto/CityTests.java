e01c0df84ecbc781585ba87b8af43f386fd3ce40
Sami Z <79335705+samizirakgamedev@users.noreply.github.com>
2022-02-27 16:11:46 +0000

    Further ConnectionManager Tests
    Completed all ConnectionManager tests and added display names for them all. Removed redundant imports. Cleaned up CityTests and removed redundant code and spaces.

--- src/test/java/com/steelswans/dto/CityTests.java
+++ src/test/java/com/steelswans/dto/CityTests.java
@@ -40,56 +40,32 @@ public class CityTests {
     public void cityTest(){
         Assertions.assertEquals("Bristol", jsonObject.get("name"));
     }
-
     @Test
     @DisplayName("Check if correct city id is returned")
     public void cityIdCheck(){
         Assertions.assertEquals((long) 2654675, jsonObject.get("id"));
     }
-
     @Test
     @DisplayName("Check if the correct timezone for Bristol, UK is returned")
     public void checkTimezone(){
         Assertions.assertEquals((long) 0, jsonObject.get("timezone"));
     }
-
     @Test
     @DisplayName("Check if the correct country for Bristol, UK is returned")
     public void checkCountry(){
         JSONObject getSys = (JSONObject) jsonObject.get("sys");
         Assertions.assertEquals("GB", getSys.get("country"));
     }
-
     @Test
     @DisplayName("Check if the correct latitude for Bristol, UK is returned")
     public void checkLatitude(){
         JSONObject getCoord = (JSONObject) jsonObject.get("coord");
         Assertions.assertEquals(51.4552, getCoord.get("lat"));
     }
-
     @Test
     @DisplayName("Check if the correct longitude for Bristol, UK is returned")
     public void checkLongitude(){
         JSONObject getCoord = (JSONObject) jsonObject.get("coord");
         Assertions.assertEquals(-2.5967, getCoord.get("lon"));
     }
-
-//    @Test
-//    @DisplayName("Check invalid input response")//doesn't work
-//    public void checkInvalidInput(){
-//        ConnectionManager cm2 = ConnectionManager.getConnection("https://api.openweathermap.org/data/2.5/weather?q=", "", APIKeyFileReader.readAPIKeyFile("apikey.txt"));
-//        Injector injector2 = new Injector();
-//        jsonObject2 = injector2.getJSONResponse(Objects.requireNonNull(cm2.getHttpResponse(cm2.makeHttpRequest())));
-//        clouds = new Clouds();
-//        coord = new Coord();
-//        main = new Main();
-//        sys = new Sys();
-//        weather = new Weather();
-//        wind = new Wind();
-//        snow = new Snow();
-//        rain = new Rain();
-//        injector2.injectIntoDTO(jsonObject2, clouds, coord, main, sys, weather, wind, snow, rain);
-    //System.out.println(jsonObject2);
-    //System.out.println(jsonObject2.get("cod"));
-//    }
 }
--- src/test/java/com/steelswans/framework/ConnectionManagerTest.java
+++ src/test/java/com/steelswans/framework/ConnectionManagerTest.java
@@ -1,38 +1,35 @@
 package com.steelswans.framework;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import java.net.URI;
-import java.net.http.HttpRequest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class ConnectionManagerTest {
     private static ConnectionManager cm;
+    // Instantiating an instance of the ConnectionManager prior to every test.
     @BeforeAll
     static void setUp(){
         cm = new ConnectionManager("https://api.openweathermap.org/data/2.5/weather?q=",
                 "bristol,gb", APIKeyFileReader.readAPIKeyFile("apikey.txt"));
     }
-
     @Test
+    @DisplayName("Given we make a HTTP Request it returns a HTTP Request as a HTTPRequest that isn't null")
     void returnHttpRequest() {
         var actualRequest = cm.returnHttpRequest();
         assertNotNull(actualRequest);
     }
-
     @Test
-    void returnStringHttpRequestClassTypeCheck() {
+    @DisplayName("Given we make a HTTP Request it returns a HTTP Request as a String that isn't null")
+    void returnStringHttpRequest() {
         var actualRequest = cm.returnStringHttpRequest();
         assertNotNull(actualRequest);
     }
-
     @Test
+    @DisplayName("Given we request a HTTP Response it returns a HTTPResponse<String> that contains the URL of our request")
     void returnHttpResponse() {
         var actualResponse = cm.returnHttpResponse(cm.returnHttpRequest());
-
         var expectedBaseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
         var expectedCity = "bristol,gb";
         var expectedAPIKey = APIKeyFileReader.readAPIKeyFile("apikey.txt");
@@ -42,26 +39,13 @@ class ConnectionManagerTest {
         assert expectedAPIKey != null;
         assertTrue(actualResponse.toString().contains(expectedAPIKey));
     }
-
     @Test
+    @DisplayName("Given we request a HTTP Response it returns a String that contains the URL of our request")
     void returnStringHttpResponse() {
-        var actualResponse = cm.returnHttpResponse(cm.returnHttpRequest());
+        var actualResponse = cm.returnStringHttpResponse(cm.returnHttpRequest());
 
-        var expectedBaseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
-        var expectedCity = "bristol,gb";
-        var expectedAPIKey = APIKeyFileReader.readAPIKeyFile("apikey.txt");
-
-        assertTrue(actualResponse.toString().contains(expectedBaseUrl));
-        assertTrue(actualResponse.toString().contains(expectedCity));
-        assert expectedAPIKey != null;
-        assertTrue(actualResponse.toString().contains(expectedAPIKey));
-    }
-
-    @Test
-    void getConnectionResponse() {
-    }
-
-    @Test
-    void getHeaders() {
+        assertTrue(actualResponse.toLowerCase().contains("bristol"));
+        assertTrue(actualResponse.toLowerCase().contains("weather"));
+        assertTrue(actualResponse.toLowerCase().contains("description"));
     }
 }
