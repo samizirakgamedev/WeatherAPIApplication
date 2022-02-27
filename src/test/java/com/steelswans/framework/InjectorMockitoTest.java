8f37434c1320ab7b1ec8948b5f2591c387bc1365
Piotr Sulek <piotr.sulek@outlook.com>
2022-02-27 16:52:52 +0000

    Cleaned up InjectorMockitoTest
    Moved repeated code to the @BeforeAll.

--- src/test/java/com/steelswans/framework/InjectorMockitoTest.java
+++ src/test/java/com/steelswans/framework/InjectorMockitoTest.java
@@ -13,6 +13,7 @@ import java.io.*;
 public class InjectorMockitoTest {
     private static JSONParser parser;
     private static String response;
+    private static JSONObject jsonObject;
 
     private static Clouds clouds;
     private static Coord coord;
@@ -43,103 +44,65 @@ public class InjectorMockitoTest {
         } catch (Exception e) {
             e.printStackTrace();
         }
+
+        ConnectionManager mockManager = Mockito.mock(ConnectionManager.class);
+        Injector injector = new Injector();
+        Mockito.when(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest())).thenReturn(response);
+        jsonObject =  injector.getJSONResponseBody(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest()));
+        injector.injectIntoDTO(jsonObject, clouds, coord, main, sys, weather, wind, snow, rain);
     }
 
     @Test
     @DisplayName("When getJSON response method is used, jsonObject should not be null")
     public void testGetJSONResponse(){
-        ConnectionManager mockManager = Mockito.mock(ConnectionManager.class);
-        Injector injector = new Injector();
-        Mockito.when(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest())).thenReturn(response);
-        JSONObject jsonObject =  injector.getJSONResponseBody(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest()));
         Assertions.assertNotNull(jsonObject);
     }
 
     @Test
     @DisplayName("When injectIntoDTO method is used, the weather object should not be null")
     public void givenInjectIntoDTO_WeatherIsNotNull(){
-        ConnectionManager mockManager = Mockito.mock(ConnectionManager.class);
-        Injector injector = new Injector();
-        Mockito.when(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest())).thenReturn(response);
-        JSONObject jsonObject =  injector.getJSONResponseBody(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest()));
-        injector.injectIntoDTO(jsonObject, clouds, coord, main, sys, weather, wind, snow, rain);
         Assertions.assertNotNull(weather);
     }
 
     @Test
     @DisplayName("When injectIntoDTO method is used, the clouds object should not be null")
     public void givenInjectIntoDTO_CloudsIsNotNull(){
-        ConnectionManager mockManager = Mockito.mock(ConnectionManager.class);
-        Injector injector = new Injector();
-        Mockito.when(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest())).thenReturn(response);
-        JSONObject jsonObject =  injector.getJSONResponseBody(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest()));
-        injector.injectIntoDTO(jsonObject, clouds, coord, main, sys, weather, wind, snow, rain);
         Assertions.assertNotNull(clouds);
     }
 
     @Test
     @DisplayName("When injectIntoDTO method is used, the coord object should not be null")
     public void givenInjectIntoDTO_CoordIsNotNull(){
-        ConnectionManager mockManager = Mockito.mock(ConnectionManager.class);
-        Injector injector = new Injector();
-        Mockito.when(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest())).thenReturn(response);
-        JSONObject jsonObject =  injector.getJSONResponseBody(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest()));
-        injector.injectIntoDTO(jsonObject, clouds, coord, main, sys, weather, wind, snow, rain);
         Assertions.assertNotNull(coord);
     }
 
     @Test
     @DisplayName("When injectIntoDTO method is used, the main object should not be null")
     public void givenInjectIntoDTO_MainIsNotNull(){
-        ConnectionManager mockManager = Mockito.mock(ConnectionManager.class);
-        Injector injector = new Injector();
-        Mockito.when(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest())).thenReturn(response);
-        JSONObject jsonObject =  injector.getJSONResponseBody(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest()));
-        injector.injectIntoDTO(jsonObject, clouds, coord, main, sys, weather, wind, snow, rain);
         Assertions.assertNotNull(main);
     }
 
     @Test
     @DisplayName("When injectIntoDTO method is used, the sys object should not be null")
     public void givenInjectIntoDTO_SysIsNotNull(){
-        ConnectionManager mockManager = Mockito.mock(ConnectionManager.class);
-        Injector injector = new Injector();
-        Mockito.when(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest())).thenReturn(response);
-        JSONObject jsonObject =  injector.getJSONResponseBody(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest()));
-        injector.injectIntoDTO(jsonObject, clouds, coord, main, sys, weather, wind, snow, rain);
         Assertions.assertNotNull(sys);
     }
 
     @Test
     @DisplayName("When injectIntoDTO method is used, the wind object should not be null")
     public void givenInjectIntoDTO_WindIsNotNull(){
-        ConnectionManager mockManager = Mockito.mock(ConnectionManager.class);
-        Injector injector = new Injector();
-        Mockito.when(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest())).thenReturn(response);
-        JSONObject jsonObject =  injector.getJSONResponseBody(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest()));
-        injector.injectIntoDTO(jsonObject, clouds, coord, main, sys, weather, wind, snow, rain);
         Assertions.assertNotNull(wind);
     }
 
     @Test
     @DisplayName("When injectIntoDTO method is used, the snow object should not be null")
     public void givenInjectIntoDTO_SnowIsNotNull(){
-        ConnectionManager mockManager = Mockito.mock(ConnectionManager.class);
-        Injector injector = new Injector();
-        Mockito.when(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest())).thenReturn(response);
-        JSONObject jsonObject =  injector.getJSONResponseBody(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest()));
-        injector.injectIntoDTO(jsonObject, clouds, coord, main, sys, weather, wind, snow, rain);
         Assertions.assertNotNull(snow);
     }
 
     @Test
     @DisplayName("When injectIntoDTO method is used, the rain object should not be null")
     public void givenInjectIntoDTO_RainIsNotNull(){
-        ConnectionManager mockManager = Mockito.mock(ConnectionManager.class);
-        Injector injector = new Injector();
-        Mockito.when(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest())).thenReturn(response);
-        JSONObject jsonObject =  injector.getJSONResponseBody(mockManager.returnStringHttpResponse(mockManager.returnHttpRequest()));
-        injector.injectIntoDTO(jsonObject, clouds, coord, main, sys, weather, wind, snow, rain);
         Assertions.assertNotNull(rain);
     }
 }
