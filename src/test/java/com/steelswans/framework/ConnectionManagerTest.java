4e04d3a86dd2d6d9d4a148e2d049a30b4628cfe2
Sami Z <79335705+samizirakgamedev@users.noreply.github.com>
2022-02-27 15:24:50 +0000

    ConnectionManagerTests
    Created tests for ConnectionManager class and more specifically for returning the HTTP Request both as a HTTPRequest and as a String. Also created a test for checking that the HTTPResponse is being returned correctly.

--- src/test/java/com/steelswans/framework/ConnectionManagerTest.java
+++ src/test/java/com/steelswans/framework/ConnectionManagerTest.java
@@ -1,5 +1,7 @@
 package com.steelswans.framework;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.net.URI;
@@ -9,23 +11,50 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ConnectionManagerTest {
     private static ConnectionManager cm;
-
-    @Test
-    void returnHttpRequest() {
-    }
-
-    @Test
-    void returnStringHttpRequestClassTypeCheck() {
-        ConnectionManager cm = new ConnectionManager("https://api.openweathermap.org/data/2.5/weather?q=",
+    @BeforeAll
+    static void setUp(){
+        cm = new ConnectionManager("https://api.openweathermap.org/data/2.5/weather?q=",
                 "bristol,gb", APIKeyFileReader.readAPIKeyFile("apikey.txt"));
     }
 
     @Test
+    void returnHttpRequest() {
+        var actualRequest = cm.returnHttpRequest();
+        assertNotNull(actualRequest);
+    }
+
+    @Test
+    void returnStringHttpRequestClassTypeCheck() {
+        var actualRequest = cm.returnStringHttpRequest();
+        assertNotNull(actualRequest);
+    }
+
+    @Test
     void returnHttpResponse() {
+        var actualResponse = cm.returnHttpResponse(cm.returnHttpRequest());
+
+        var expectedBaseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+        var expectedCity = "bristol,gb";
+        var expectedAPIKey = APIKeyFileReader.readAPIKeyFile("apikey.txt");
+
+        assertTrue(actualResponse.toString().contains(expectedBaseUrl));
+        assertTrue(actualResponse.toString().contains(expectedCity));
+        assert expectedAPIKey != null;
+        assertTrue(actualResponse.toString().contains(expectedAPIKey));
     }
 
     @Test
     void returnStringHttpResponse() {
+        var actualResponse = cm.returnHttpResponse(cm.returnHttpRequest());
+
+        var expectedBaseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+        var expectedCity = "bristol,gb";
+        var expectedAPIKey = APIKeyFileReader.readAPIKeyFile("apikey.txt");
+
+        assertTrue(actualResponse.toString().contains(expectedBaseUrl));
+        assertTrue(actualResponse.toString().contains(expectedCity));
+        assert expectedAPIKey != null;
+        assertTrue(actualResponse.toString().contains(expectedAPIKey));
     }
 
     @Test
